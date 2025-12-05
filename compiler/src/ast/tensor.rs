7273002a2a0fdebabb787da4c9ace33202c96590
Jordy Aaldering <aalderingjordy@gmail.com>
2025-12-05 15:15:43 +0100

    problem written out

--- compiler/src/ast/tensor.rs
+++ compiler/src/ast/tensor.rs
@@ -13,6 +13,25 @@ use super::{AstConfig, ArgOrVar, Block};
 /// the index vector, but it must at least be of the same shape.)
 ///
 /// For now, we assume only a single partition.
+///
+/// ```
+/// fn scope(int ub) -> int[ub] {
+///     one = 1;
+///     two = 2;
+///     ret = { a = { jv + one | jv < ub };
+///             b = { jv + two | jv < ub };
+///             ub + one + a[iv] + b[iv]    // a and b can be found in the first scope
+///                                         // one cannot be found, so go one scope up
+///                                         // even there, ub cannot be found, so go up again and check args
+///           | iv < ub
+///     };
+///     return ret;
+/// }
+/// ```
+///
+/// This means we might have to look through multiple scopes.
+/// The naive approach of passing the fundef to traversal functions in not enough
+/// we might have to look through a number of scopes.
 #[derive(Clone, Debug)]
 pub struct Tensor<Ast: AstConfig> {
     pub body: Block<Ast>,
