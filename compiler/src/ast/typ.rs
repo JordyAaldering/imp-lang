3428bc5a91c778bd59b29f1bc4ca1f4d9f3f9c12
Jordy Aaldering <aalderingjordy@gmail.com>
2025-12-10 11:21:15 +0100

    fmt type node

--- compiler/src/ast/typ.rs
+++ compiler/src/ast/typ.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Clone, Debug)]
 pub struct Type {
     pub basetype: BaseType,
@@ -14,4 +16,30 @@ pub enum BaseType {
 pub enum Shape {
     Scalar,
     Vector(String),
+}
+
+impl fmt::Display for Type {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}{}", self.basetype, self.shp)
+    }
+}
+
+impl fmt::Display for BaseType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use BaseType::*;
+        match self {
+            U32 => write!(f, "u32"),
+            Bool => write!(f, "bool"),
+        }
+    }
+}
+
+impl fmt::Display for Shape {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use Shape::*;
+        match self {
+            Scalar => Ok(()),
+            Vector(n) => write!(f, "[{}]", n),
+        }
+    }
 }
