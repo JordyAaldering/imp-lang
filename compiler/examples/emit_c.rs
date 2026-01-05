07a5b155849c2ad3149f1b18fa43a38117c21dc9
Jordy Aaldering <aalderingjordy@gmail.com>
2026-01-05 16:00:00 +0100

    print parsing ast

--- compiler/examples/emit_c.rs
+++ compiler/examples/emit_c.rs
@@ -6,6 +6,7 @@ fn main() {
     let file = env::args().nth(1).unwrap();
     let src = fs::read_to_string(&file).unwrap();
     let ast = scanparse::scanparse(&src).unwrap();
+    println!("{}", ast);
     let ast = convert_to_ssa::convert_to_ssa(ast);
     show::Show::new(Box::new(io::stdout())).show_program(&ast).unwrap();
     let ast = type_infer::TypeInfer::new().trav_program(ast).unwrap();
--- compiler/src/scanparse/parse_ast.rs
+++ compiler/src/scanparse/parse_ast.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use crate::ast::{Bop, Type, Uop};
 
 #[derive(Debug)]
@@ -42,4 +44,66 @@ pub enum Expr {
     Identifier(String),
     Bool(bool),
     U32(u32),
+}
+
+impl fmt::Display for Program {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for fundef in &self.fundefs {
+            writeln!(f, "{}", fundef)?;
+        }
+        Ok(())
+    }
+}
+
+impl fmt::Display for Fundef {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let args = self.args.iter()
+            .map(|(ty, id)| {
+                format!("{} {}", ty, id)
+            })
+            .collect::<Vec<String>>()
+            .join(", ");
+
+        writeln!(f, "fn {}({}) -> {} {{", self.id, args, self.ret_type)?;
+
+        for stmt in &self.body {
+            writeln!(f, "    {}", stmt)?;
+        }
+
+        writeln!(f, "    return {};", self.ret_expr)?;
+
+        writeln!(f, "}}")
+    }
+}
+
+impl fmt::Display for Stmt {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use Stmt::*;
+        match self {
+            Assign { lhs, expr } => {
+                write!(f, "{} = {};", lhs, expr)
+            },
+        }
+    }
+}
+
+impl fmt::Display for Expr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use Expr::*;
+        match self {
+            Tensor { iv, expr, lb, ub } => {
+                write!(f, "{{ {} | {} <= {} < {} }}", expr, lb, iv, ub)
+            },
+            Binary { l, r, op } => {
+                write!(f, "({} {} {})", l, op, r)
+            },
+            Unary { r, op } => {
+                write!(f, "{}{}", op, r)
+            },
+            // Terminals
+            Identifier(v) => write!(f, "{}", v),
+            Bool(v) => write!(f, "{}", v),
+            U32(v) => write!(f, "{}", v),
+        }
+    }
 }
