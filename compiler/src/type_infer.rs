b1d1c2bce82b96ff5e7028ba7f67343ea001968c
Jordy Aaldering <33897257+JordyAaldering@users.noreply.github.com>
2026-04-01 20:53:49 +0200

    simplify type inference traversal

--- compiler/src/type_infer.rs
+++ compiler/src/type_infer.rs
@@ -2,10 +2,6 @@ use std::collections::HashMap;
 
 use crate::{ast::*, traverse::Traverse};
 
-/// Type inference pass: transforms UntypedAst to TypedAst.
-///
-/// Walks the AST computing types for all expressions, variables, and function returns.
-/// Creates new Avis entries for each SSA binding with properly inferred types.
 pub fn type_infer<'ast>(program: Program<'ast, UntypedAst>) -> Result<Program<'ast, TypedAst>, InferenceError> {
     Ok(TypeInfer::new().trav_program(program))
 }
@@ -39,23 +35,16 @@ impl<'ast> TypeInfer<'ast> {
     fn alloc_expr(&self, expr: Expr<'ast, TypedAst>) -> &'ast Expr<'ast, TypedAst> {
         Box::leak(Box::new(expr))
     }
-
-    fn find_local_def(&self, key: &'ast Avis<UntypedAst>) -> LocalDef<'ast, UntypedAst> {
-        find_local_in_scopes(&self.scopes, key).expect("missing local definition in type inference")
-    }
-
-    /// Get the type of an SSA value (must be TypedAst after pass_id).
-    fn type_of(&self, id: Id<'ast, TypedAst>) -> Type {
-        match id {
-            Id::Arg(i) => self.args[i].ty.clone().unwrap(),
-            Id::Var(v) => v.ty.clone(),
-        }
-    }
 }
 
 impl<'ast> Traverse<'ast> for TypeInfer<'ast> {
     type InAst = UntypedAst;
+
     type OutAst = TypedAst;
+
+    ///
+    /// Declarations
+    ///
 
     fn trav_fundef(&mut self, fundef: Fundef<'ast, Self::InAst>) -> Fundef<'ast, Self::OutAst> {
         self.args = fundef.args.clone();
@@ -65,11 +54,12 @@ impl<'ast> Traverse<'ast> for TypeInfer<'ast> {
         self.idmap.clear();
         self.new_ids.clear();
 
-        let ret = self.trav_id(fundef.ret_id());
+        let (ret, _) = self.trav_id(fundef.ret_id());
 
-        let new_args = self.args.iter().map(|arg| {
-            self.alloc_avis(arg.name.clone(), arg.ty.clone().unwrap())
-        }).collect::<Vec<_>>();
+        let mut new_args = Vec::with_capacity(fundef.args.len());
+        for arg in fundef.args {
+            new_args.push(self.trav_farg(arg));
+        }
 
         self.scopes.pop().unwrap();
         let mut body = self
@@ -96,6 +86,14 @@ impl<'ast> Traverse<'ast> for TypeInfer<'ast> {
         self.alloc_avis(arg.name.clone(), arg.ty.clone().unwrap())
     }
 
+    fn trav_vardec(&mut self, _: &'ast Avis<Self::InAst>) -> &'ast Avis<Self::OutAst> {
+        unreachable!("Vardecs should be replaced manually")
+    }
+
+    ///
+    /// Statements
+    ///
+
     fn trav_stmt(&mut self, stmt: Stmt<'ast, Self::InAst>) -> Stmt<'ast, Self::OutAst> {
         match stmt {
             Stmt::Assign(assign) => Stmt::Assign(self.trav_assign(assign)),
@@ -116,71 +114,50 @@ impl<'ast> Traverse<'ast> for TypeInfer<'ast> {
     }
 
     fn trav_return(&mut self, ret: Return<'ast, Self::InAst>) -> Return<'ast, Self::OutAst> {
-        Return { id: self.trav_id(ret.id) }
+        let (id, _) = self.trav_id(ret.id);
+        Return { id }
     }
 
-    fn trav_id(&mut self, id: Id<'ast, Self::InAst>) -> Id<'ast, Self::OutAst> {
-        match id {
-            Id::Arg(i) => Id::Arg(i),
-            Id::Var(old) => {
-                if let Some(new_id) = self.idmap.get(&(old as *const _)) {
-                    return Id::Var(*new_id);
-                }
+    ///
+    /// Expressions
+    ///
 
-                match self.find_local_def(old) {
-                    LocalDef::Assign(old_expr) => {
-                        let new_expr = self.trav_expr(old_expr.clone());
-                        let new_ty = self.type_of_expr(&new_expr);
+    type ExprOut = (Expr<'ast, Self::OutAst>, Type);
 
-                        let new_id = self.alloc_avis(old.name.clone(), new_ty);
-                        self.idmap.insert(old as *const _, new_id);
-                        self.new_ids.push(new_id);
-
-                        let expr_ref = self.alloc_expr(new_expr);
-                        self.new_ssa.last_mut().unwrap().push(ScopeEntry::Assign {
-                            avis: new_id,
-                            expr: expr_ref,
-                        });
-
-                        Id::Var(new_id)
-                    }
-                    LocalDef::IndexRange { lb, ub } => {
-                        let lb = self.trav_id(lb);
-                        let ub = self.trav_id(ub);
-
-                        let new_id = self.alloc_avis(old.name.clone(), Type::scalar(BaseType::U32));
-                        self.idmap.insert(old as *const _, new_id);
-                        self.new_ids.push(new_id);
-                        self.new_ssa.last_mut().unwrap().push(ScopeEntry::IndexRange {
-                            iv: new_id,
-                            lb,
-                            ub,
-                        });
-
-                        Id::Var(new_id)
-                    }
-                }
-            }
-        }
-    }
-
-    fn trav_expr(&mut self, expr: Expr<'ast, Self::InAst>) -> Expr<'ast, Self::OutAst> {
+    fn trav_expr(&mut self, expr: Expr<'ast, Self::InAst>) -> Self::ExprOut {
         use Expr::*;
         match expr {
-            Tensor(n) => Tensor(self.trav_tensor(n)),
-            Binary(n) => Binary(self.trav_binary(n)),
-            Unary(n) => Unary(self.trav_unary(n)),
-            Bool(n) => Bool(self.trav_bool(n)),
-            U32(n) => U32(self.trav_u32(n)),
+            Tensor(n) => {
+                let (expr, ty) = self.trav_tensor(n);
+                (Tensor(expr), ty)
+            },
+            Binary(n) => {
+                let (expr, ty) = self.trav_binary(n);
+                (Binary(expr), ty)
+            },
+            Unary(n) => {
+                let (expr, ty) = self.trav_unary(n);
+                (Unary(expr), ty)
+            },
+            Bool(n) => {
+                let (expr, ty) = self.trav_bool(n);
+                (Bool(expr), ty)
+            },
+            U32(n) => {
+                let (expr, ty) = self.trav_u32(n);
+                (U32(expr), ty)
+            },
         }
     }
 
-    fn trav_tensor(&mut self, tensor: Tensor<'ast, Self::InAst>) -> Tensor<'ast, Self::OutAst> {
+    type TensorOut = (Tensor<'ast, Self::OutAst>, Type);
+
+    fn trav_tensor(&mut self, tensor: Tensor<'ast, Self::InAst>) -> Self::TensorOut {
         self.scopes.push(tensor.scope_block());
         self.new_ssa.push(Vec::new());
 
-        let lb = self.trav_id(tensor.lb);
-        let ub = self.trav_id(tensor.ub);
+        let (lb, _check_if_vec) = self.trav_id(tensor.lb);
+        let (ub, _check_if_vec) = self.trav_id(tensor.ub);
 
         let iv_new = self.alloc_avis(tensor.iv.name.clone(), Type::scalar(BaseType::U32));
         self.idmap.insert(tensor.iv as *const _, iv_new);
@@ -191,7 +168,7 @@ impl<'ast> Traverse<'ast> for TypeInfer<'ast> {
             ub,
         });
 
-        let ret = self.trav_id(tensor.ret);
+        let (ret, _ret_ty) = self.trav_id(tensor.ret);
 
         self.scopes.pop().unwrap();
         let body = self
@@ -205,81 +182,91 @@ impl<'ast> Traverse<'ast> for TypeInfer<'ast> {
             })
             .collect::<Vec<_>>();
 
-        Tensor { iv: iv_new, lb, ub, ret, body }
+        let tensor = Tensor { iv: iv_new, lb, ub, ret, body };
+        (tensor, Type::vector(BaseType::U32, "."))
     }
 
-    fn trav_binary(&mut self, binary: Binary<'ast, Self::InAst>) -> Binary<'ast, Self::OutAst> {
-        let l = self.trav_id(binary.l);
-        let r = self.trav_id(binary.r);
+    type BinaryOut = (Binary<'ast, Self::OutAst>, Type);
 
-        Binary { l, r, op: binary.op }
+    fn trav_binary(&mut self, binary: Binary<'ast, Self::InAst>) -> Self::BinaryOut {
+        let (l, l_ty) = self.trav_id(binary.l);
+        let (r, r_ty) = self.trav_id(binary.r);
+        let ty = unifies(l_ty, r_ty).unwrap();
+        (Binary { l, r, op: binary.op }, ty)
     }
 
-    fn trav_unary(&mut self, unary: Unary<'ast, Self::InAst>) -> Unary<'ast, Self::OutAst> {
-        let r = self.trav_id(unary.r);
-        Unary { r, op: unary.op }
+    type UnaryOut = (Unary<'ast, Self::OutAst>, Type);
+
+    fn trav_unary(&mut self, unary: Unary<'ast, Self::InAst>) -> Self::UnaryOut {
+        let (r, r_ty) = self.trav_id(unary.r);
+        (Unary { r, op: unary.op }, r_ty)
     }
 
-    fn trav_bool(&mut self, value: bool) -> bool {
-        value
-    }
+    ///
+    /// Terminals
+    ///
 
-    fn trav_u32(&mut self, value: u32) -> u32 {
-        value
-    }
+    type IdOut = (Id<'ast, Self::OutAst>, Type);
 
-    type StmtOut = Stmt<'ast, Self::OutAst>;
+    fn trav_id(&mut self, id: Id<'ast, Self::InAst>) -> Self::IdOut {
+        match id {
+            Id::Arg(i) => {
+                let ty = self.args[i].ty.clone().unwrap();
+                (Id::Arg(i), ty)
+            },
+            Id::Var(old) => {
+                if let Some(new_id) = self.idmap.get(&(old as *const _)) {
+                    let ty = new_id.ty.clone();
+                    return (Id::Var(*new_id), ty);
+                }
 
-    type AssignOut = Assign<'ast, Self::OutAst>;
+                match find_local_in_scopes(&self.scopes, old).unwrap() {
+                    LocalDef::Assign(old_expr) => {
+                        let (new_expr, new_ty) = self.trav_expr(old_expr.clone());
 
-    type ReturnOut = Return<'ast, Self::OutAst>;
+                        let new_id = self.alloc_avis(old.name.clone(), new_ty.clone());
+                        self.idmap.insert(old as *const _, new_id);
+                        self.new_ids.push(new_id);
 
-    type ExprOut = Expr<'ast, Self::OutAst>;
+                        let expr_ref = self.alloc_expr(new_expr);
+                        self.new_ssa.last_mut().unwrap().push(ScopeEntry::Assign {
+                            avis: new_id,
+                            expr: expr_ref,
+                        });
 
-    type TensorOut = Tensor<'ast, Self::OutAst>;
+                        (Id::Var(new_id), new_ty)
+                    }
+                    LocalDef::IndexRange { lb, ub } => {
+                        let (lb, _) = self.trav_id(lb);
+                        let (ub, _) = self.trav_id(ub);
 
-    type BinaryOut = Binary<'ast, Self::OutAst>;
+                        let new_id = self.alloc_avis(old.name.clone(), Type::scalar(BaseType::U32));
+                        self.idmap.insert(old as *const _, new_id);
+                        self.new_ids.push(new_id);
+                        self.new_ssa.last_mut().unwrap().push(ScopeEntry::IndexRange {
+                            iv: new_id,
+                            lb,
+                            ub,
+                        });
 
-    type UnaryOut = Unary<'ast, Self::OutAst>;
-
-    type IdOut = Id<'ast, Self::OutAst>;
-
-    type BoolOut = bool;
-
-    type U32Out = u32;
-}
-
-impl<'ast> TypeInfer<'ast> {
-    /// Compute the type of a typed expression.
-    fn type_of_expr(&self, expr: &Expr<'ast, TypedAst>) -> Type {
-        match expr {
-            Expr::Tensor(_) => {
-                // Tensor type shape is determined at render time
-                Type::vector(BaseType::U32, ".")
-            }
-            Expr::Binary(Binary { l, r, op }) => {
-                let lty = self.type_of(*l);
-                let rty = self.type_of(*r);
-                let basety = unifies(lty, rty).unwrap();
-
-                use Bop::*;
-                match op {
-                    Add | Sub | Mul | Div => Type { basetype: BaseType::U32, shp: basety.shp },
-                    Eq | Ne => Type { basetype: BaseType::Bool, shp: basety.shp },
-                    Lt | Le | Gt | Ge => Type { basetype: BaseType::Bool, shp: basety.shp },
+                        let ty = Type::scalar(BaseType::U32);
+                        (Id::Var(new_id), ty)
+                    }
                 }
             }
-            Expr::Unary(Unary { r, op }) => {
-                let rty = self.type_of(*r);
-                use Uop::*;
-                match op {
-                    Neg => Type { basetype: BaseType::U32, shp: rty.shp },
-                    Not => Type { basetype: BaseType::Bool, shp: rty.shp },
-                }
-            }
-            Expr::Bool(_) => Type::scalar(BaseType::Bool),
-            Expr::U32(_) => Type::scalar(BaseType::U32),
         }
+    }
+
+    type BoolOut = (bool, Type);
+
+    fn trav_bool(&mut self, value: bool) -> Self::BoolOut {
+        (value, Type::scalar(BaseType::Bool))
+    }
+
+    type U32Out = (u32, Type);
+
+    fn trav_u32(&mut self, value: u32) -> Self::U32Out {
+        (value, Type::scalar(BaseType::U32))
     }
 }
 
