e21d448928f0a7f3f6ffeaf6a9e8a2b2edff38d4
Jordy Aaldering <33897257+JordyAaldering@users.noreply.github.com>
2026-04-01 18:28:58 +0200

    cleanup

--- compiler/examples/emit.rs
+++ compiler/examples/emit.rs
@@ -1,21 +1,25 @@
-use compiler::{traverse::Traverse, *};
+use compiler::*;
 
 use std::{env, fs};
 
 fn main() {
     let file = env::args().nth(1).unwrap();
     let src = fs::read_to_string(&file).unwrap();
+
+    println!("=== scanparse ===");
     let ast = scanparse::scanparse(&src).unwrap();
     println!("{}", ast);
+
+    println!("=== convert_to_ssa ===");
     let ast = convert_to_ssa::convert_to_ssa(ast);
     println!("{}", show::show(&ast));
+
+    println!("=== type_infer ===");
     let ast = type_infer::type_infer(ast).unwrap();
     println!("{}", show::show(&ast));
 
+    println!("=== codegen_c ===");
     let mut codegen = compile::codegen_c::CodegenContext::new();
-    let ast = codegen.trav_program(ast);
+    let _ast = codegen.trav_program(ast);
     print!("{}", codegen.output);
-
-    let ast = undo_ssa::UndoSsa::new().trav_program(&ast);
-    println!("{:?}", ast);
 }
