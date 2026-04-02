f84a3d56c3ae54a06379efa615c064b9df41ac16
Jordy Aaldering <33897257+JordyAaldering@users.noreply.github.com>
2026-04-02 15:52:45 +0200

    split traversals into subphases

--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -1,34 +1,35 @@
 #![feature(associated_type_defaults)]
 
 pub mod ast;
-pub mod compile;
-pub mod flatten;
-pub mod convert_to_ssa;
-pub mod scanparse;
-pub mod show;
 pub mod traverse;
-pub mod type_infer;
+pub mod show;
+// Compiler phases
+pub mod scp;
+pub mod pre;
+pub mod tc;
+pub mod opt;
+pub mod cg;
 
 pub use crate::traverse::{Traverse, Visit};
 
 use crate::ast::*;
 
 pub fn compile(src: &str) -> Program<'static, TypedAst> {
-    let ast = scanparse::scanparse(&src).unwrap();
-    let ast = flatten::flatten(ast);
-    let ast = convert_to_ssa::convert_to_ssa(ast);
-    let ast = type_infer::type_infer(ast).unwrap();
+    let ast = scp::scanparse(&src).unwrap();
+    let ast = pre::flatten(ast);
+    let ast = pre::convert_to_ssa(ast);
+    let ast = tc::type_infer(ast).unwrap();
     ast
 }
 
 pub fn emit_ffi(ast: &Program<'static, TypedAst>, outfile: &str) {
-    let mut cg = compile::codegen_ffi::CompileFfi::new();
+    let mut cg = cg::codegen_ffi::CompileFfi::new();
     cg.visit_program(&ast);
     std::fs::write(outfile, cg.finish()).unwrap();
 }
 
 pub fn emit_c(ast: &Program<'static, TypedAst>, outfile: &str) {
-    let mut cg = compile::codegen_c::CompileC::new();
+    let mut cg = cg::codegen_c::CompileC::new();
     cg.visit_program(ast);
     std::fs::write(outfile, cg.finish()).unwrap();
 }
---
+++ compiler/src/pre.rs
@@ -0,0 +1,5 @@
+mod flatten;
+mod convert_to_ssa;
+
+pub use flatten::flatten;
+pub use convert_to_ssa::convert_to_ssa;
