af7b352695552733f8f653f215b62df34c06c7e5
Jordy Aaldering <33897257+JordyAaldering@users.noreply.github.com>
2026-06-03 10:00:48 +0200

    tests

--- stdlib/src/lib.rs
+++ stdlib/src/lib.rs
@@ -37,4 +37,29 @@ mod tests {
         assert_eq!(arr.shp, vec![n]);
         assert_eq!(arr.data, (0..n).collect::<Vec<_>>());
     }
+
+    #[parameterized(n = { 0, 1, 10 })]
+    fn test_scalar_add(n: usize) {
+        let arr = iota(n);
+        let one = ImpArray::scalar(1usize);
+        let arr = add_usize_usize(arr, one);
+        assert_eq!(arr.shp, vec![n]);
+        assert_eq!(arr.data, (1..=n).collect::<Vec<_>>());
+    }
+
+    #[parameterized(n = { 0, 1, 10 })]
+    fn test_sum(n: usize) {
+        let arr = iota(n);
+        let v = sum_usize(arr);
+        assert_eq!(v, (0..n).sum());
+    }
+
+    #[parameterized(n = { 0, 1, 10 })]
+    fn test_prod(n: usize) {
+        let arr = iota(n);
+        let one = ImpArray::scalar(1usize);
+        let arr = add_usize_usize(arr, one);
+        let v = sum_usize(arr);
+        assert_eq!(v, (1..=n).sum());
+    }
 }
