c5a77cc4143746b5c59170e13b385f3fc724d82a
Jordy Aaldering <33897257+JordyAaldering@users.noreply.github.com>
2026-09-08 14:28:44 +0200

    setup passing debugging argument

--- imp_lang/src/main.rs
+++ imp_lang/src/main.rs
@@ -3,10 +3,15 @@ use std::path::PathBuf;
 use clap::Parser;
 
 #[derive(Parser)]
-#[derive(Default)]
 pub struct Options {
     #[arg(short('b'), long("break"))]
     pub b: Option<imp_lang::Phase>,
+
+    /// Print debug information for the given [`imp_lang::Phase`]
+    ///
+    /// (Currently not yet used)
+    #[arg(short('d'), long("debug"), value_delimiter(','))]
+    pub d: Vec<imp_lang::Phase>,
 
     #[arg(short('o'), long("out"))]
     pub outdir: Option<PathBuf>,
